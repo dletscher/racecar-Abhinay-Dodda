3de8593d3be35fd06ada4b4be41891ee807dec87
Abhinay-Dodda <doddaabhinay@gmail.com>
2025-06-10 11:27:23 -0500

    My Agent version 3.0

--- MyAgent.py
+++ MyAgent.py
@@ -8,11 +8,11 @@ class Agent:
         l45=lidar[0]
         c=lidar[2]
         r45=lidar[4]
-        h=0.7 + 0.8 * vel
+        h=0.7 + vel + 0.2 *(min(l45,r45))
 
-        if r45-l45>0.8:
+        if r45-l45>0.2:
             dir='right'
-        elif l45-r45>0.8:
+        elif l45-r45>0.2:
             dir='left'
         else:
             dir='straight'
